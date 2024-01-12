ac3aa7da5588137a051e49d375f941c4c6f27b90
WaseeAhsan <132597375+WaseeAhsan@users.noreply.github.com>
2024-01-12 11:20:18 +0600

    Add files via upload

---
+++ Data Structure/Sqrt root Decomposition.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+
+int main(){
+
+    int n ; cin>> n  ; 
+
+    vector<int>a(n) ; 
+
+    for(int i = 0 ; i< n ;i++)
+    {
+        cin >> a[i] ; 
+    }
+
+    int len  =  sqrtl( n ) + 1  ; 
+    vector<int>b(len ) ; 
+
+    for(int i = 0 ; i < n ; i++ )
+    {
+        b[i / len ] += a[i] ; 
+    }
+
+    for(int i = 0 ; i < len ;i++)
+    {
+       // cout << b[i] << " " ; 
+    }
+cout << endl ; 
+        int q;
+    cin >> q;
+
+    while(q--) {
+        int l, r;
+        cin >> l >> r;
+        l--; r--;
+
+        int sum = 0;
+
+        for(int i = l; i <= r;) {
+            if(i % len == 0 && i + len - 1 <= r) {
+                sum += b[i / len];
+                i += len;
+            }
+            else {
+                sum += a[i];
+                i++;
+            }
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
